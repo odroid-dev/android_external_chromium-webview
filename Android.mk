df4af844a90ede7182e2b30b74aa9d9379cf1a1e
secuflag <secuflag@ticino.com>
2019-10-07 09:43:14 +0200

    Fix makefile

--- Android.mk
+++ Android.mk
@@ -28,8 +28,7 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm
-LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_MODULE_TARGET_ARCH := arm arm64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
 
